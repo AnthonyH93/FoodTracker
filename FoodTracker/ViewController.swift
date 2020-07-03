5ab75bddd11210422839074ea6b44c34d7c6ebda
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2020-07-03 14:24:41 -0400

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Anthony Hopkins on 2020-07-03.
+//  Copyright © 2020 Anthony Hopkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
